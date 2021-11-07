886a5fb7d7c0ae0596be43a79e1fde26279c0378
Vlad Zemlyanskov <vlad.zemlyanskov.adci@gmail.com>
2021-11-08 03:46:51 +0600

    Create Navigation component. Add images for Navigation. Change base styles. Add npm prop-types.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
 import React from 'react';
+import Navbar from './components/common/Navbar';
 
 function App() {
   return (
     <div className="App">
-
-      <header className="App-header">
-        Hello React
-      </header>
-
-      <h1>Hello React</h1>
-
-
-      <br/>
-      <a target="_blank" rel="noreferrer" href="https://www.google.ru/">qweqweq</a>
-      <br/>
-
-      <ul>
-        <li>1231</li>
-        <li>123</li>
-        <li>123</li>
-      </ul>
-      <br/>
-      <h1>qwe</h1>
-      <h2>qwe</h2>
-      <h3>qwe</h3>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/common/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import logoImg from '../../../images/logo.svg';
+import siginImg from '../../../images/sign.svg';
+
+import './styles.scss';
+
+const Navbar = () => {
+
+  return (
+    <>
+      <header className="navigation">
+        <div className="navigation-logo">
+          <img className="navigation-logo__img" src={logoImg} alt="Logo" />
+          <h1 className="navigation-logo__title">MrDonald's</h1>
+        </div>
+        <button className="navigation-login">
+          <img src={siginImg} alt="login" />
+          <p>войти</p>
+        </button>
+      </header>
+    </>
+  )
+}
+
+export default Navbar;
